b1c7adcee7c9f14287a1645bd50713406d111856
Kirk Strobeck <kirk@strobeck.com>
2021-10-09 14:38:43 -0700

    progress

---
+++ src/components/button/__automated__/index.stories.tsx
@@ -0,0 +1,4 @@
+import { runner } from '@automated/automated/dist/storybook/index';
+import automated from '../__automated';
+
+runner(automated);
---
+++ src/components/button/__automated__/index.test.tsx
@@ -0,0 +1,4 @@
+import { runner } from '@automated/automated/dist/jest/index';
+import automated from '../__automated';
+
+runner(automated);
